c75de90fa06afc72b4b2eaebf053536e11d0d1b2
João Felipe <jfk.abrao@hotmail.com>
2024-12-13 23:05:35 -0300

    v7
    score points + sync score points + points UI + points UI sync

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : NetworkBehaviour
 {
-
-    private Dictionary<ulong, NetworkVariable<int>> playerScore;
-
     public static GameController Instance { get; private set; }
+
+    public event Action<int> OnPlayer1ScoreChanged;
+    public event Action<int> OnPlayer2ScoreChanged;
+
+
+    private NetworkVariable<int> player1Score = new(0);
+    private NetworkVariable<int> player2Score = new(0);
+
 
     private void Awake()
     {
@@ -15,9 +23,41 @@ public class GameController : NetworkBehaviour
         {
             Instance = this;
         }
-
-        playerScore = new Dictionary<ulong, NetworkVariable<int>>();
     }
+
+    public override void OnNetworkSpawn()
+    {
+        player1Score.OnValueChanged += Player1Score_OnValueChanged;
+        player2Score.OnValueChanged += Player2Score_OnValueChanged;
+
+
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += NetworkManager_OnClientConnectedCallback;
+        }
+    }
+
+
+    private void NetworkManager_OnClientConnectedCallback(ulong clientId)
+    {
+        print("Player " + clientId + " has joined the game");
+    }
+
+
+    #region On Value Changed
+
+    private void Player1Score_OnValueChanged(int previousValue, int newValue)
+    {
+        OnPlayer1ScoreChanged?.Invoke(newValue);
+    }
+
+    private void Player2Score_OnValueChanged(int previousValue, int newValue)
+    {
+        OnPlayer2ScoreChanged?.Invoke(newValue);
+    }
+
+    #endregion
+
 
     #region Spawn/ Destroy Methods
 
@@ -37,23 +77,25 @@ public class GameController : NetworkBehaviour
 
     public void HitSpawnableObject(SpawnableObject spawnableObject)
     {
-
         ScorePointServerRpc(spawnableObject.GetSpawnableObjectSO().pointsValue);
-
-
 
         GameControllerMultiplayer.Instance.DestroySpawnableObject(spawnableObject);
     }
 
-
-
     [ServerRpc(RequireOwnership = false)]
     private void ScorePointServerRpc(int pointsToScore, ServerRpcParams serverRpcParams = default)
     {
-        playerScore[serverRpcParams.Receive.SenderClientId].Value += pointsToScore;
-
-        Debug.Log(playerScore[serverRpcParams.Receive.SenderClientId].Value + " of player " + serverRpcParams.Receive.SenderClientId);
+        if(serverRpcParams.Receive.SenderClientId == NetworkManager.ServerClientId)
+        {
+            //Host score point
+            player1Score.Value += pointsToScore;
+        } else
+        {
+            //Client score point
+            player2Score.Value += pointsToScore;
+        }
     }
 
     #endregion
+
 }
--- Assets/Scripts/UI/PlayersScoreUI.cs
+++ Assets/Scripts/UI/PlayersScoreUI.cs
@@ -1,8 +1,31 @@
 using TMPro;
+using Unity.Netcode;
 using UnityEngine;
-public class PlayersScoreUI : MonoBehaviour
+public class PlayersScoreUI : NetworkBehaviour
 {
     [SerializeField] private TextMeshProUGUI player1ScoreText;
     [SerializeField] private TextMeshProUGUI player2ScoreText;
 
+
+    private void Start()
+    {
+        GameController.Instance.OnPlayer1ScoreChanged += GameController_OnPlayer1ScoreChanged; ;
+        GameController.Instance.OnPlayer2ScoreChanged += GameController_OnPlayer2ScoreChanged; ;
+    }
+
+    private void GameController_OnPlayer1ScoreChanged(int scorePlayer1)
+    {
+        if(IsHost)
+            player1ScoreText.text = scorePlayer1.ToString();
+        else
+            player2ScoreText.text = scorePlayer1.ToString();
+    }
+
+    private void GameController_OnPlayer2ScoreChanged(int scorePlayer2)
+    {
+        if (IsHost)
+            player2ScoreText.text = scorePlayer2.ToString();
+        else
+            player1ScoreText.text = scorePlayer2.ToString();
+    }
 }
